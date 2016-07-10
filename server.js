283189748e517548430ad9c724268a997bdef742
narutoadi <mis.ad1206@gmail.com>
2016-07-10 14:05:04 +0530

    Start using .gitignore

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var express = require('express'),
 // start webserver on port 8080
 var server =  http.createServer(app);
 var io = socketIo.listen(server);
-server.listen(8080);
+var port = process.env.PORT || 3000;
+server.listen(port);
 // add directory with our static files
 app.use(express.static(__dirname + '/public'));
 console.log("Server running on 127.0.0.1:8080");
