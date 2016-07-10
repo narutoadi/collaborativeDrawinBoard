89ed595849409927b317ae9d388f93ac66c70b8b
narutoadi <mis.ad1206@gmail.com>
2016-07-10 12:35:09 +0530

    DrawingBoard is ready

---
+++ public/client.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function() {
+   var mouse = { 
+      click: false,
+      move: false,
+      pos: {x:0, y:0},
+      pos_prev: false
+   };
+   // get canvas element and create context
+   var canvas  = document.getElementById('drawing');
+   var context = canvas.getContext('2d');
+   var width   = window.innerWidth;
+   var height  = window.innerHeight;
+   var socket  = io.connect();
+   var but = document.getElementById('clear');
+   // set canvas to full browser width/height
+   canvas.width = width;
+   canvas.height = height;
+
+   // register mouse event handlers
+   canvas.onmousedown = function(e){ mouse.click = true; };
+   canvas.onmouseup = function(e){ mouse.click = false; };
+
+   canvas.onmousemove = function(e) {
+      // normalize mouse position to range 0.0 - 1.0
+      mouse.pos.x = e.clientX / width;
+      mouse.pos.y = e.clientY / height;
+      mouse.move = true;
+   };
+
+   but.onclick =  function(e){
+		socket.emit('clearit', true);
+	}
+   // draw line received from server
+	socket.on('draw_line', function (data) {
+      var line = data.line;
+      context.beginPath();
+      context.moveTo(line[0].x * width, line[0].y * height);
+      context.lineTo(line[1].x * width, line[1].y * height);
+      context.stroke();
+   });
+
+	socket.on('clearit', function(){
+			context.clearRect(0,0,width, height);
+			console.log("client clearit");
+	});
+
+	
+   
+   // main loop, running every 25ms
+   function mainLoop() {
+      // check if the user is drawing
+      if (mouse.click && mouse.move && mouse.pos_prev) {
+         // send line to to the server
+         socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
+         mouse.move = false;
+      }
+      mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+      setTimeout(mainLoop, 25);
+   }
+   mainLoop();
+});
