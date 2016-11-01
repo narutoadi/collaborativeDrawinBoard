767d57244aba8ca18dbb6146c00e956ec7f6431b
narutoadi <mis.ad1206@gmail.com>
2016-11-01 13:40:52 +0530

    added unique color and username for every connection.

--- public/client.js
+++ public/client.js
@@ -12,6 +12,10 @@ document.addEventListener("DOMContentLoaded", function() {
    var height  = window.innerHeight;
    var chatWidth = width*0.45;
    var socket  = io.connect();
+   var color   = getRandomColor();
+   // Setting drawing style
+   
+
  //  var but = document.getElementById('clear');
    // set canvas to full browser width/height
    canvas.width = width;
@@ -37,30 +41,42 @@ document.addEventListener("DOMContentLoaded", function() {
       var line = data.line;
       
       context.beginPath();
+      context.strokeStyle = data.color;
+      context.lineCap = 'round';
+      context.lineJoin = 'round';
+      context.lineWidth =  8;
       context.moveTo(line[0].x * width , line[0].y * height);
       context.lineTo(line[1].x * width, line[1].y * height);
       context.stroke();
    });
    var name = '';
    $('form').submit(function(){
-      socket.emit('chat message',{'name': name, 
+      socket.emit('chat message',{'name': name,
+         'color': color, 
          'msg':$('#m').val()});
    // socket.emit('chat message', $('#m').val());
       $('#m').val('');
       return false;
    });
    socket.on('start', function(data){
-      $('#messages').append($('<li>').text(data.jsonmsg));
+      //$('#messages').css('color', data.color);
+
+      $('#messages').append($('<li>')
+         .text(data.jsonmsg)
+         .css('color', data.color));
    });
    socket.on('finish', function(data){
       $('#messages').append($('<li>').text(data.jsonmsg));
    });
    socket.on('chat message', function(data){
-      $('#messages').append($('<li>').text(data.name+": "+data.msg));
+      //$('#messages').css('color', data.color);
+       $('#messages').append($('<li>')
+         .text(data.name+" : "+data.msg)
+         .css('color', data.color));
    });
    while (name == '') {
                name = prompt("What's your name?","");
-               socket.emit('register', name );
+               socket.emit('register', { 'name': name, 'color': color } );
             }
 
 
@@ -72,18 +88,30 @@ document.addEventListener("DOMContentLoaded", function() {
 //  location.reload();
    width   = window.innerWidth;
    height  = window.innerHeight;
+   chatWidth = width*0.45;
+   canvas.width = width;
+   canvas.height = height;
    context.clearRect(0,0,width, height);
-   console.log("client clearit");
+//   console.log("client clearit");
    socket.emit('redraw');
 });
 	
+   // function to generate random color
+   function getRandomColor() {
+      var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i=0; i<6; i++){
+         color += letters[Math.floor(Math.random()*16)];
+      }
+      return color;
+   }
    
    // main loop, running every 25ms
    function mainLoop() {
       // check if the user is drawing
       if (mouse.click && mouse.move && mouse.pos_prev) {
          // send line to to the server
-         socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
+         socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ], color: [ color ] });
          mouse.move = false;
       }
       mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express = require('express'),
 
 // start webserver on port 8080
 var server =  http.createServer(app);
+// io will handle all the clients connected to the server.
 var io = socketIo.listen(server);
 var port = process.env.PORT || 8080;
 server.listen(port);
@@ -14,15 +15,17 @@ console.log("Server running on 127.0.0.1:8080");
 
 // array of all lines drawn
 var line_history = [];
+// array of colors of lines
+var color_history = [];
 
 // event-handler for new incoming connections
 io.on('connection', function (socket) {
   socket.emit('chat message', {'name': 'Server', 'msg': 'You Are Welcome !'});
-  socket.on('register',function(name){
-  io.emit('start',{'jsonmsg':''+name+' is now CONNECTED. Say Hello!'});
+  socket.on('register',function(data){
+  io.emit('start',{'jsonmsg':''+data.name+' is now CONNECTED. Say Hello!', 'color': data.color});
   });
   socket.on('chat message', function(data){
-    io.emit('chat message', {'name': data.name,'msg':data.msg});
+    io.emit('chat message', {'name': data.name,'msg':data.msg, 'color':data.color });
   });
   console.log('a user connected');
   socket.on('disconnect', function(){
@@ -31,15 +34,16 @@ io.on('connection', function (socket) {
   });
    // first send the history to the new client
    for (var i in line_history) {
-      socket.emit('draw_line', { line: line_history[i] } );
+      socket.emit('draw_line', { line: line_history[i], color: color_history[i] } );
    }
 
    // add handler for message type "draw_line".
    socket.on('draw_line', function (data) {
       // add received line to history 
       line_history.push(data.line);
+      color_history.push(data.color);
       // send line to all clients
-      io.emit('draw_line', { line: data.line });
+      io.emit('draw_line', { line: data.line, color: data.color });
    });
   // socket.on('clearit', function(){
   // 	line_history = [];
@@ -47,7 +51,7 @@ io.on('connection', function (socket) {
   // });
   socket.on('redraw', function (){
     for (var i in line_history) {
-      socket.emit('draw_line', { line: line_history[i] } );
+      socket.emit('draw_line', { line: line_history[i], color: color_history[i] } );
    }
  });
 });
